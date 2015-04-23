a33d72242d75c84dbde45ba4cf9fe6e31561ead6
Scott Maddox <smaddox@utexas.edu>
2015-04-22 19:20:10 -0500

    add `__init__` module

---
+++ fdint/__init__.py
@@ -0,0 +1,11 @@
+'''
+Precise and fast Fermi-Dirac integrals of integer and half integer order.
+    
+[1] T. Fukushima, "Precise and fast computation of Fermi-Dirac integral
+    of integer and half integer order by piecewise minimax rational
+    approximation," Applied Mathematics and Computation, vol. 259,
+    pp. 708-729, May 2015.
+'''
+from .fdint import *
+
+__all__ = ['fdk', 'dfdk']
