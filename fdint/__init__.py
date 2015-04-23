ac5157fb7b9a5f1572fd373b969957338169caa6
Scott Maddox <smaddox@utexas.edu>
2015-04-22 19:43:43 -0500

    add copyright headers

--- fdint/__init__.py
+++ fdint/__init__.py
@@ -1,3 +1,22 @@
+#
+#   Copyright (c) 2015, Scott J. Maddox
+#
+#   This file is part of Fermi-Dirac Integrals (FDINT).
+#
+#   FDINT is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published
+#   by the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   FDINT is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with FDINT.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 '''
 Precise and fast Fermi-Dirac integrals of integer and half integer order.
     
